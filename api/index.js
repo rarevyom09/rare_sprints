272c5f89c5cd1c75fb28c3b380f14631271df490
Vyom Padalia <104118297+rarevyom09@users.noreply.github.com>
2023-10-28 20:32:53 +0530

    Update index.js f

--- api/index.js
+++ api/index.js
@@ -14,21 +14,21 @@ require('dotenv').config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://rare-sprints.vercel.app/');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://rare-sprints.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', true);
 
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.use(cors({
-  origin: 'https://rare-sprints.vercel.app/',
+  origin: 'https://rare-sprints.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
