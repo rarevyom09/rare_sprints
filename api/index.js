b69cb37b0c55a75612e0f5bd6ac4fe2f1dc6384b
Vyom Padalia <104118297+rarevyom09@users.noreply.github.com>
2023-10-28 13:51:12 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'sprintsbyvyompadalia.vercel.app' }));
 app.use(express.json());
 app.use(cookieParser()); // Use cookie-parser
 
