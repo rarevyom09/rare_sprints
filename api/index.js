d83c1ae64789dc983a629acf8344d7da5574f10b
Vyom Padalia <104118297+rarevyom09@users.noreply.github.com>
2023-10-28 15:00:30 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
   credentials: true,
   origin: 'https://rare-sprints.vercel.app',
-  methods: '*'
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(express.json());
 app.use(cookieParser()); // Use cookie-parser
