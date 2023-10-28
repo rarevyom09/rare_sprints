0dcc6b9e1d281085ece97b99e50ff40659979984
Vyom Padalia <104118297+rarevyom09@users.noreply.github.com>
2023-10-28 14:24:10 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const LoginPage = () => {
     try {
       setLoading(true); // Start loading
 
-      const response = await fetch('https://sprintsbyvyompadalia.vercel.app/login', {
+      const response = await fetch('https://sprintsbyvyompadalia-rarevyom09.vercel.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
